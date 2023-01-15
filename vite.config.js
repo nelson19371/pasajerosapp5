c84edd5fcd139747453042a315467272c00dc1e6
nelsondev <sandoval19371@gmail.com>
2023-01-15 14:42:46 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: [
-  //       "json-as-xlsx",
+  build: {
+    rollupOptions: {
+      external: [
+        "json-as-xlsx",
         
-  //     ],
-  //   },
-  // },
+      ],
+    },
+  },
 })
